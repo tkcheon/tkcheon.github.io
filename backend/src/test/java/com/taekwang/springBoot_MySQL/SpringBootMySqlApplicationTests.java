d5ae94d33587e90a08624fdf74b28be8ad8427b7
tkcheon <100704194+tkcheon@users.noreply.github.com>
2024-11-12 17:18:58 +0900

    backend directory create

---
+++ backend/src/test/java/com/taekwang/springBoot_MySQL/SpringBootMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taekwang.springBoot_MySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
